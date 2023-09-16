28a0f9302b444c2f6649fa8073a350c01cf1cd77
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-09-16 10:56:36 +0200

    Update models.py and main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from API.models import Result, Violation
+from API.models import Result, Violation, Rule
 
 app = FastAPI()
 @app.post("/post")
 async def upload_code(item: str):
-    violation = Violation("Violation 1", "Wrong dependencies", "Medium", "Architecture", "public static...")
+    rule = Rule("Dependency","Wrong dependency", True)
+    violation = Violation("Violation 1", "Wrong dependencies", "Medium", "Architecture", "public static...", rule)
     result = Result(0.2,[violation])
     return {"result": result}
