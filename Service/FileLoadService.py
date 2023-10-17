5426bd8cd21931448a0d35bf94edce80370784f6
Sombrero <adriana@grecea.net>
2023-10-17 23:25:17 +0200

    BPR-87: cleanup

--- Service/FileLoadService.py
+++ Service/FileLoadService.py
@@ -1,17 +1,10 @@
 from tkinter import filedialog as fd
 import os
-import re
 from CSVFileService import *
 from TermFrequencyService import *
 
-
-#TODO: add filter to ignore assembly files and migrations
-#TODO: refactor regex for usings and method identification
 def process_data_from_folder():
-    filePathName = []
-    fileName = []
     folder = fd.askdirectory()
-    exclude = {'.git', '.idea', 'bin', 'obj'}
     fors = 0
     ifs = 0
     whiles = 0
@@ -27,23 +20,6 @@ def process_data_from_folder():
 
     row = [fors,foreaches,ifs,whiles]
     write_row_to_csv(row)
-    # for root,dirs,files in os.walk(folder):
-    #     dirs[:] = [d for d in dirs if d not in exclude]
-    #
-    #     for file_name in files:
-    #         if file_name.endswith('.cs'):
-    #             file_rel_path = os.path.relpath(os.path.join(root, file_name), folder).replace("\\", "/")
-    #             filePathName.append(file_rel_path)
-    #
-    #             fileName.append(file_name)
-    #
-    #             with open(os.path.join(root,file_name), 'r') as file:
-    #                 code = file.read()
-    #                 method_matches = re.findall(method_pattern, code)
-    #                 usings_matches = re.findall(usings_pattern,code,re.MULTILINE)
-    #             parent_dir = os.path.basename(os.path.dirname(file_rel_path))
-    #             row = [parent_dir, file_rel_path, file_name, method_matches, usings_matches]
-    #             write_row_to_csv(row)
 
 process_data_from_folder()
 
