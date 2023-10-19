b09b2c45866d2624842f3d5d753acebb8e3e9d95
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-10-19 13:04:10 +0200

    BPR-89: Update main

--- main.py
+++ main.py
@@ -9,20 +9,3 @@ async def upload_code(item: str):
     violation = Violation("Violation 1", "Wrong dependencies", "Medium", "Architecture", "public static...", rule)
     result = Result(0.2,[violation])
     return {"result": result}
-
-import json
-
-j = {"cycle": "4.8.1", "releaseDate": "2022-08-09", "eol": "false", "lts": "false"}
-class FrameworkDetails:
-    def __init__(self, cycle, releaseDate, eol, lts):
-        self.cycle = cycle
-        self.releaseDate = releaseDate
-        self.eol = eol
-        self.lts = lts
-class Payload(object):
-    def __init__(self, j):
-        self.__dict__ = json.loads(j)
-
-p = Payload(j)
-
-print(p.data)
