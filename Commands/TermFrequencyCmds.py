ac81882c3e7d0c074dbfe37cd96175b1c3456e0b
Sombrero <adriana@grecea.net>
2023-10-22 22:18:45 +0200

    BPR-91: refactored the term frequency into commands

---
+++ Commands/TermFrequencyCmds.py
@@ -0,0 +1,31 @@
+import re
+from Helpers.RegexHelper import IF_PATTERN,FOREACH_PATTERN,FOR_PATTERN,WHILE_PATTERN
+from Commands.CommandModel import Command
+class ForFrequencyCommand(Command):
+    def execute(self, file_name: str) -> int:
+        with open(file_name, 'r', encoding='utf8', errors='ignore') as file:
+            code = file.read()
+            matches = re.findall(FOR_PATTERN, code, re.MULTILINE)
+            return len(matches)
+
+class ForEachFrequencyCommand(Command):
+    def execute(self, file_name: str) -> int:
+        with open(file_name, 'r', encoding='utf8', errors='ignore') as file:
+            code = file.read()
+            matches = re.findall(FOREACH_PATTERN, code, re.MULTILINE)
+            return len(matches)
+
+
+class IfFrequencyCommand(Command):
+    def execute(self, file_name: str) -> int:
+        with open(file_name, 'r', encoding='utf8', errors='ignore') as file:
+            code = file.read()
+            for_matches = re.findall(IF_PATTERN, code, re.MULTILINE)
+            return len(for_matches)
+
+class WhileFrequencyCommand(Command):
+    def execute(self, file_name: str) -> int:
+        with open(file_name, 'r', encoding='utf8', errors='ignore') as file:
+            code = file.read()
+            matches = re.findall(WHILE_PATTERN, code, re.MULTILINE)
+            return len(matches)
