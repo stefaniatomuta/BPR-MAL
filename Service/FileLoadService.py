b659447a2284d48cf3e7b4def10c83974b818030
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-10-26 22:20:59 +0200

    BPR-92: Update FileLoadService.py to return groupped commands

--- Service/FileLoadService.py
+++ Service/FileLoadService.py
@@ -1,20 +1,11 @@
 from tkinter import filedialog as fd
-import os
-from CSVFileService import *
 from Commands.FrameworkCmd import FrameworkCommand
 from Commands.TermFrequencyCmds import *
 from Commands.CodeBreakdownCmds import *
 from Commands.InheritanceOverheadCmds import *
-from Service import FrameworksService
-from Service.InheritanceService import get_max_inheritance_depth
-
-def get_commands():
-from Service import EOLService
 from Helpers.GitIgnoreHelper import *
 from Commands.MetricsCmd import *
-from collections import defaultdict
-import inspect
-def process_data_from_folder():
+def get_commands():
     commands = []
     commands.append(MetricsCommand())
     commands.append(FrameworkCommand())
@@ -34,9 +25,8 @@ def process_data_from_folder():
 def process_data_from_folder():
     commands = get_commands()
     folder = fd.askdirectory()
-    sums = defaultdict(int)
     sums = {}
-    class_matches = []
+    dict_matches = {}
     csfiles = 0
     # for entry in os.scandir(folder):
     #     if entry.name == '.gitignore':
@@ -50,17 +40,20 @@ def process_data_from_folder():
             if file_name.endswith('.cs'):
                 csfiles += 1
             for command in commands:
-                file_path = os.path.join(root,file_name)
+                command_name = type(command).__name__
+                file_path = os.path.join(root, file_name)
                 analysis_results = command.execute(file_path)
-                for file, match in analysis_results.items():
-                    info = FrameworksService.EOL_API(match)
-                    print(info.isEndOfLife)
                 if(isinstance(analysis_results, list)):
-                    class_matches.extend(analysis_results)
+                    if len(analysis_results) != 0:
+                        if command_name not in dict_matches:
+                            dict_matches[command_name] = []
+                        dict_matches[command_name].extend(analysis_results)
+
                 if(isinstance(analysis_results, int)):
-                    command_name = type(command).__name__
-                    sums[command_name] = sums.get(str(command_name),0)+ analysis_results
-    print("inheritance tree max depth: " + repr(get_max_inheritance_depth(class_matches)))
+                    sums[command_name] = sums.get(str(command_name), 0) + analysis_results
+
+    # print("inheritance tree max depth: " + repr(get_max_inheritance_depth(class_matches)))
+    print(dict_matches)
     print(sums)
 
 process_data_from_folder()
