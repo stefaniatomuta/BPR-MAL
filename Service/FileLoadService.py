638824648b73fd9f5c940521de2a7db7e7facd1d
Sombrero <adriana@grecea.net>
2023-10-30 11:52:03 +0100

    BPR-93: refactor inheritance

--- Service/FileLoadService.py
+++ Service/FileLoadService.py
@@ -6,6 +6,8 @@ from Commands.InheritanceOverheadCmds import *
 from Helpers.GitIgnoreHelper import *
 from Commands.MetricsCmd import *
 from Commands.CallsToExternalProvidersCmds import *
+from InheritanceService import *
+from CodeBreakdownService import *
 def get_commands():
     commands = []
     commands.append(MetricsCommand())
@@ -20,7 +22,7 @@ def get_commands():
     commands.append(ClassNumberCommand())
     commands.append(InterfaceNumberCommand())
     commands.append(InheritanceDeclarationsCommand())
-    commands.append(ClassInheritanceCommand())
+    # commands.append(ClassInheritanceCommand())
     commands.append(ExternalAPICallsCommand())
     return commands
 
@@ -29,15 +31,16 @@ def process_data_from_folder():
     folder = fd.askdirectory()
     sums = {}
     dict_matches = {}
+    class_matches = []
     csfiles = 0
-
     for root, dirs, files in os.walk(folder):
         for file_name in files:
+            file_path = os.path.join(root, file_name)
             if file_name.endswith('.cs'):
                 csfiles += 1
+                class_matches.extend(get_matches_in_file(file_path,CLASS_PATTERN))
             for command in commands:
                 command_name = type(command).__name__
-                file_path = os.path.join(root, file_name)
                 analysis_results = command.execute(file_path)
                 if(isinstance(analysis_results, list)):
                     if len(analysis_results) != 0:
@@ -48,7 +51,7 @@ def process_data_from_folder():
                 if(isinstance(analysis_results, int)):
                     sums[command_name] = sums.get(str(command_name), 0) + analysis_results
 
-    # print("inheritance tree max depth: " + repr(get_max_inheritance_depth(class_matches)))
+    print("inheritance tree max depth: " + repr(get_max_inheritance_depth(class_matches)))
     print(dict_matches)
     print(sums)
 
