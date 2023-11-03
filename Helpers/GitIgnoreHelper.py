5fd9484ebcd8396eb7cb84e8d4baa0db23d43b57
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-11-03 22:47:50 +0100

    BPR-93: Update gitignore

--- Helpers/GitIgnoreHelper.py
+++ Helpers/GitIgnoreHelper.py
@@ -1,23 +1,15 @@
-import os
 import fnmatch
 import re
 
 
-def read_gitignore(file_name) -> list:
+def read_gitignore() -> list:
     gitignore_content = []
     modified_lines = []
-    with open(file_name, 'r') as file:
+    with open('../Config/.gitignore', 'r') as file:
         lines = file.readlines()
         for line in lines:
             if line.startswith('['):
-                regex = r'\[([^\]]+)\](.*)'
-                match = re.search(regex, line)
-                first_letter = match.group(1)[0]
-                second_letter = match.group(1)[1]
-                upperCase = first_letter + match.group(2)
-                modified_lines.append(upperCase)
-                lowerCase = second_letter + match.group(2)
-                modified_lines.append(lowerCase)
+                process_square_bracket_line(line, modified_lines)
             else:
                 modified_line = extract_git_ignore_files(line)
             if modified_line != line and modified_line is not None:
@@ -30,10 +22,19 @@ def read_gitignore(file_name) -> list:
     return modified_lines
 
 
+def process_square_bracket_line(line, modified_lines):
+    match = re.match(r'\[([^\]]+)\](.*)', line)
+    if match:
+        first_letter, rest_of_line = match.group(1)[0], match.group(2)
+        modified_lines.extend([first_letter + rest_of_line, first_letter.lower() + rest_of_line])
+
+
 def extract_git_ignore_files(line: str) -> str:
     if line.startswith('*'):
         index = line.find('*')
         line = line[index + 1:].strip()
+        if line.endswith('/\n'):
+            line = line.rstrip('/\n')
         return line
 
 
@@ -41,11 +42,17 @@ def is_ignored(file_name, gitignore_content):
     for pattern in gitignore_content:
         if file_name.startswith(pattern):
             return True
+        if file_name.endswith(('.dll', '.md', '.sln',
+                               '.DotSettings.user', '.css', '.js',
+                               '.xml', '.nupkg')):
+            return True
     return False
 
 
 def should_ignore_dir(dir_path, gitignore_content):
     for pattern in gitignore_content:
-        if dir_path.startswith(pattern):
+        if pattern.endswith('/'):
+            pattern = pattern.rstrip('/')
+        if fnmatch.fnmatch(dir_path, pattern):
             return True
     return False
