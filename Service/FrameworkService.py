b8c192729459aed60525007d484803362f39b4cd
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-11-18 21:18:01 +0100

    BPR-122: Added relative path for dictionaries

--- Service/FrameworkService.py
+++ Service/FrameworkService.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 from Helpers.RegexHelper import *
@@ -10,11 +11,12 @@ def get_matches(file_name) -> dict:
     if file_name.endswith('.csproj'):
         with open(file_name, 'r', encoding='utf8') as f:
             code = f.read()
+            file_base_name = os.path.relpath(file_name)
             for pattern in patterns:
                 matches = re.findall(pattern, code)
                 for match in matches:
                     info = EOL_API(match)
-                    file_matches[file_name] = {'version': match, 'status': info.isEndOfLife}
+                    file_matches[file_base_name] = {'version': match, 'status': info.isEndOfLife}
     return file_matches
 
 
--- Service/ModuleCouplingService.py
+++ Service/ModuleCouplingService.py
@@ -1,5 +1,6 @@
 import re
 from Helpers.RegexHelper import *
+from Service.CodeSimilarityService import *
 
 
 def class_coupling_mapping(files) -> dict:
@@ -20,12 +21,13 @@ def class_coupling_mapping(files) -> dict:
     return class_coupling_list
 
 
-def class_coupling_listing(files) -> list:
-    class_coupling_list = []
+def class_coupling_listing(files) -> dict:
+    class_coupling_list = {}
     for file_name in files:
         if file_name.endswith('.cs'):
-            with open(file_name, 'r',encoding='utf8',errors='ignore') as f:
+            with open(file_name, 'r', encoding='utf8', errors='ignore') as f:
                 code = f.read()
+                file_base_name = os.path.relpath(file_name)
                 class_name = r'class (\w+)?'
                 class_match = re.findall(class_name, code)
                 if len(class_match) != 0:
@@ -34,5 +36,5 @@ def class_coupling_listing(files) -> list:
                     for match in matches:
                         if match not in list:
                             list.append(match)
-                    class_coupling_list.append(len(list))
+                    class_coupling_list[file_base_name] = (len(list))
     return class_coupling_list
