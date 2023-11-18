70ec6faae4f5146af12172d452d7193ceb093a74
Sombrero <adriana@grecea.net>
2023-11-18 13:17:46 +0100

    BPR-121: refactor csv service

--- Service/FileLoadService.py
+++ Service/FileLoadService.py
@@ -3,7 +3,7 @@ from CodeSimilarityService import get_code_similarity_values
 
 def write_result(result, rules):
     result = process_code_similarity(result)
-    write_columns_to_csv(rules)
+    write_column_headers_to_csv(rules)
     write_to_csv(result)
     print(result)
 
