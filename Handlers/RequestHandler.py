491d275b3c0477b51ef0c493643aba66ef9491be
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-11-29 11:39:32 +0100

    Fixed codesimilarity

--- Handlers/RequestHandler.py
+++ Handlers/RequestHandler.py
@@ -2,15 +2,17 @@ from Service.ExtractionService import *
 from Service.FileLoadService import *
 from Service.CommandHandlerService import *
 
+TrainModel = False
 
 ##TODO: once the ML part is ready add a service that deals with that and is called here to return the value together with the rest
 def process_request(folder_path, request_rules):
     extraction_result, all_rules = process_data_from_folder(folder_path)
     intact_extraction = extraction_result
     processed_result = filter_result(intact_extraction, request_rules)
-    if processed_result:
+    if processed_result.get('codesimilarity'):
         processed_result['codesimilarity'] = filter_code_similarity(processed_result['codesimilarity'])
-    write_result(extraction_result, all_rules)
+    if TrainModel:
+        write_result(extraction_result, all_rules)
     return processed_result
 
 def automate_data_insertion(root_folder):
@@ -19,6 +21,3 @@ def automate_data_insertion(root_folder):
         extraction_result, all_rules = process_data_from_folder(folder_path)
         write_result(extraction_result,all_rules)
 
-# automate_data_insertion(r"path here")
-
-process_request(r"path here",[])
