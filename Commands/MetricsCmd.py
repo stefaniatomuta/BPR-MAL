47f3320b8fda4093da65fcd99bc52bb9f01a8d1c
Stefania Gabriela Tomuta <304173@via.dk>
2023-12-08 09:18:35 +0100

    BPR-158: Added fix for filtering out empty dict

--- Commands/MetricsCmd.py
+++ Commands/MetricsCmd.py
@@ -1,5 +1,7 @@
 from Commands.CommandModel import *
 from Service.ModuleCouplingService import *
+
+
 class ClassCouplingListingCommand(FilesRootCommand):
     def execute(self, folder_path, files: list) -> dict:
         return class_coupling_listing(folder_path, files)
